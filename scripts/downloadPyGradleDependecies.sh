05936b4c4edef4d7a8d22f7ee3efe3e422e8295c
algo <alghoshal@gmail.com>
2024-09-01 16:10:45 +0530

    extract var

--- scripts/downloadPyGradleDependecies.sh
+++ scripts/downloadPyGradleDependecies.sh
@@ -2,54 +2,57 @@
 
 export JAVA_HOME=~/Tools/jdk1.8.0_151
 
-mkdir -p ~/.gradle/pygradle-repo
+pygradle_repo=~/.gradle/pygradle-repo
+workspace=~/Workspaces
+
+mkdir -p $pygradle_repo/
 
 # Download jars
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ alabaster:0.7.1
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ atomicwrites:1.0.0
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ attrs:17.4.0
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ babel:1.3
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ certifi:2017.4.17
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ chardet:2.3.0
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ colorama:0.3.5
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ configparser:3.5.0
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ coverage:4.4
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ docutils:0.11
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ execnet:1.1
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ flake8:3.6.0
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ funcsigs:1.0.2
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ idna:2.5
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ imagesize:1.4.1
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ Jinja2:2.3
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ mccabe:0.6.0
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ more-itertools:4.0.0
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ ordereddict:1.1
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ packaging:19.2
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ pathlib2:2.2.0
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ pip:18.1
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ pluggy:0.7.1
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ py:1.11.0
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ pycodestyle:2.4.0
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ pyflakes:2.0.0
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ Pygments:2.0
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ pyparsing:2.1.9
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ pytest:3.10.0
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ pytest-cov:2.6.0
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ pytest-forked:1.6.0
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ pytest-xdist:1.24.0
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ python-dateutil:2.6.0
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ pytz:2016.4
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ requests:2.0.0
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ requests:2.20.0
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ scandir:1.10.0
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ setuptools:45.2.0
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ setuptools-git:1.2
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ six:1.16.0
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ snowballstemmer:1.1.0
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ Sphinx:1.8.1
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ sphinxcontrib
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ websupport:1.1.2
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ typing:3.5.2
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ urllib3:1.21.1
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ virtualenv:16.0.0
-$JAVA_HOME/bin/java -jar ~/Workspaces/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo ~/.gradle/pygradle-repo/ wheel:0.31.1
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ alabaster:0.7.1
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ atomicwrites:1.0.0
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ attrs:17.4.0
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ babel:1.3
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ certifi:2017.4.17
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ chardet:2.3.0
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ colorama:0.3.5
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ configparser:3.5.0
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ coverage:4.4
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ docutils:0.11
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ execnet:1.1
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ flake8:3.6.0
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ funcsigs:1.0.2
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ idna:2.5
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ imagesize:1.4.1
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ Jinja2:2.3
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ mccabe:0.6.0
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ more-itertools:4.0.0
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ ordereddict:1.1
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ packaging:19.2
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ pathlib2:2.2.0
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ pip:18.1
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ pluggy:0.7.1
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ py:1.11.0
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ pycodestyle:2.4.0
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ pyflakes:2.0.0
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ Pygments:2.0
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ pyparsing:2.1.9
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ pytest:3.10.0
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ pytest-cov:2.6.0
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ pytest-forked:1.6.0
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ pytest-xdist:1.24.0
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ python-dateutil:2.6.0
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ pytz:2016.4
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ requests:2.0.0
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ requests:2.20.0
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ scandir:1.10.0
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ setuptools:45.2.0
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ setuptools-git:1.2
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ six:1.16.0
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ snowballstemmer:1.1.0
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ Sphinx:1.8.1
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ sphinxcontrib
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ websupport:1.1.2
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ typing:3.5.2
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ urllib3:1.21.1
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ virtualenv:16.0.0
+$JAVA_HOME/bin/java -jar $workspace/pygradle/build/pivy-importer/libs/pivy-importer-0.12.11-SNAPSHOT-all.jar --repo $pygradle_repo/ wheel:0.31.1
